e2b8c7061fe30ad96b6058c843a5689fd84ab3db
Animesh <animeshagarwal19@gmail.com>
2021-07-05 19:31:00 +0530

    fifth commit

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-btjz^yz39t&kej!ps=fj$w_!rg1$4cgory$%0bi(ae^8sgl0_i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
